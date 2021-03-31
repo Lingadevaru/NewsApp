973067bc2b5d15d8fb5e933c4a51b26d015bcfea
Dev <lingadevaruc5@gmail.com>
2021-03-31 09:25:24 +0530

    NewsApp commit

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object{
+        const val API_KEY = "e2fb143850514ef385cc509f9e54e067"
+        const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
+    }
+}
